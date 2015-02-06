7ca07881fea63b33ee9bf3d8f7aa10ccf9e4c3ec
JesterXL <jesse@jessewarden.com>
2015-02-06 10:18:06 -0500

    basic build, clean, copy

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	del = require('del'),
+	browserSync = require('browser-sync'),
+  	reload = browserSync.reload;
 
-gulp.task('default', function()
-{
-	console.log("supppp.");
+var files = {
+	index: './src/index.html'
+};
+
+gulp.task('clean', function (cb) {
+  del(['build'], {force: true}, cb);
 });
+
+gulp.task('copy-index', function () {
+  return gulp.src(files.index)
+    .pipe(gulp.dest('./build'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('default', ['clean', '6to5']);
